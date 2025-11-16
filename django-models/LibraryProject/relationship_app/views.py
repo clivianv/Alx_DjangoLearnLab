4badf86eba158e2647049886298ba8a56b5065fe
clivian <clivianvoster@gmail.com>
2025-11-16 19:20:19 +0300

    changes on views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,20 +1,28 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render
 from django.views.generic import DetailView
 from .models import Book, Library
 
+
+# -----------------------------------------
+# FUNCTION-BASED VIEW (FBV)
+# -----------------------------------------
 
 def list_books(request):
     """
     Displays all books and their authors.
     """
     books = Book.objects.all()
-    return render(request, "list_books.html", {"books": books})
+    return render(request, "relationship_app/list_books.html", {"books": books})
 
+
+# -----------------------------------------
+# CLASS-BASED VIEW (CBV)
+# -----------------------------------------
 
 class LibraryDetailView(DetailView):
     """
     Displays details of a specific library and its books.
     """
     model = Library
-    template_name = "library_detail.html"
+    template_name = "relationship_app/library_detail.html"
     context_object_name = "library"
